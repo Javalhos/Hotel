79f8c444056a36c41710e1a3936741ed29f44b7c
Tatiana Vitorello <vitorellotts@gmail.com>
2019-06-05 22:39:04 -0300

    Criação dos arquivos dos Controllers

---
+++ Controllers/AccomodationController.java
@@ -0,0 +1,5 @@
+package Controllers;
+
+public class AccomodationController {
+	// TODO
+}
--- Controllers/RoomController.java
+++ Controllers/RoomController.java
@@ -1,5 +1,5 @@
 package Controllers;
 
-public class UserController {
+public class RoomController {
 	// TODO
 }
